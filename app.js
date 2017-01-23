251eeba3551afb4f02bece9c81e2a3289385f6a1
sdaitzman <sam@daitzman.com>
2017-01-23 13:31:38 -0500

    adds test code

---
+++ app.js
@@ -0,0 +1,14 @@
+const five = require('johnny-five')
+const chipio = require('chip-io')
+
+var board = new five.Board({
+  io: new chipio()
+})
+
+board.on('ready', function() {
+  // Create an LED on the PWM0 pin
+  var led = new five.Led('PWM0')
+
+  // Make it pulse with a 1 second interval
+  led.pulse(1000)
+})
