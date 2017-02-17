3a25ba391e4f69c8d296d1bf5eca922c400fbfcc
sdaitzman <sam@daitzman.com>
2017-02-17 13:17:57 -0500

    sets up chip

--- setup-chip.sh
+++ setup-chip.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-sudo apt-get -y update && sudo apt-get -y dist-upgrade && curl -sL https://deb.nodesource.com/setup_7.x | bash - && apt-get install -y nodejs git build-essential
+sudo nmcli d wifi connect "SSID" password "PASSWORD" ifname wlan0
+# sudo apt-get -y update
+# sudo apt-get -y dist-upgrade
+sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
+sudo apt-get install -y nodejs git build-essential tmux htop
+git clone https://github.com/sdaitzman/chip-io-test.git
+sudo wget -O/etc/init.d/axp209-nolim http://fordsfords.github.io/axp209-nolim/axp209-nolim
+sudo chmod +x /etc/init.d/axp209-nolim
+sudo update-rc.d axp209-nolim defaults
+sudo shutdown -r now
